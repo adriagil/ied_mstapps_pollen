bf8c7ab2d4dad0d76789bdbb694d5d69be0e20ca
adriagil <gilcoa@gmail.com>
2015-05-18 21:25:34 +0200

    initial Plants List view

--- app/scripts/views/PlantsList.js
+++ app/scripts/views/PlantsList.js
@@ -22,27 +22,11 @@
         var html = Handlebars.compile(template);
         that.$el.html(html);
 
-        $(".pollen-map-container img").on("click", function(){
+        /*$(".pollen-map-container img").on("click", function(){
           that.displayPopup();
-        });
-
-        $(".pollen-map-container img").on("mouseover", function(){
-          /*$(this).css("opacity","0.5");
-          $(this).css("border","5px solid red");*/
-          $(this).addClass("highlighted");
-        });
-
-        $(".pollen-map-container img").on("mouseout", function(){
-          /*$(this).css("opacity","1");
-          $(this).css("border","none");*/
-          $(this).removeClass("highlighted");
-        });
+        });*/
       });
       return this;
-    },
-
-    displayPopup: function() {
-      alert("you've just clicked an image!");
     }
   });
 
